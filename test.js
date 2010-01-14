b17f78fe9414169203d7389359d7a524af096deb
Joel Rosario <joel@joel-rosario.(none)>
2010-01-14 11:48:23 +0530

    Mixed sys and monads into the global namespace.

--- test.js
+++ test.js
@@ -1,26 +1,25 @@
-var sys = require('sys');
-var monads = require('./monads');
+process.mixin(GLOBAL, require('sys'), require('./monads'));
 
-sys.puts(monads._(monads.maybe_m,
+puts(domonad(maybe_m,
 		[{a: function (s) { return 10; }},
 		 {b: function (s) { return 21 + s.a; }},
 		 {c: function (s) { return 1; }}],
 		function (s) { return s.a + s.b + s.c; }));
 
-sys.puts(monads._(monads.maybe_m,
+puts(domonad(maybe_m,
 		[{a: function (s) { return 10; }},
 		 {b: function (s) { return null; }},
 		 {c: function (s) { return 1; }}],
 		function (s) { return s.a + s.b + s.c; }));
 
-sys.puts(monads._(monads.sequence_m,
+puts(domonad(sequence_m,
 		[{a: function (s) { return [1,2,3]; }},
 		 {b: function (s) { return [4,5,6]; }}],
 		function (s) { return s.a + s.b; }));
 
-maybe_sequence_m = monads.maybe_t(monads.sequence_m);
+maybe_sequence_m = maybe_t(sequence_m);
 
-sys.puts(monads._(maybe_sequence_m,
+puts(domonad(maybe_sequence_m,
 		[{a: function (s) { return [1,2,null]; }},
 		 {b: function (s) { return [4,5,6]; }}],
 		function (s) { return s.a + s.b; }));
